c4d69d9b86e192fea667c6564f9757d88891773b
Reinaldo <reinaldopared@gmail.com>
2023-12-05 12:32:38 -0400

    Updated calculator

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-// Get the element with ID "result" and assign it to the variable "display"
-var display = document.getElementById("result");
+let display = document.getElementById("display");
 
-// Create a function named "addToDisplay" that takes a parameter "value"
-function addToDisplay(value) {
-	// Append the value of the parameter "value" to the value of the "display" variable
-	display.value += value;
-}
+let buttons = Array.from(document.getElementsByClassName("button"));
 
-// Create a function named "calculate"
-function calculate() {
-	// Try to elevate the value of the "display" variable using the eval() function and assign it back to the "display" variable
-	try {
-		display.value = eval(display.value);
-	} catch (error) {
-		// If an error occur during the evaluation, assing the string "Error" to the "display"
-		display.value = "Error";
-	}
-}
-
-// Create a function named "clearDisplay"
-function clearDisplay() {
-	// Assign an empty string to the value of the "display" variable, effectively clearing it
-	display.value = "";
-}
+buttons.map((button) => {
+	button.addEventListener("click", (e) => {
+		switch (e.target.innerText) {
+			case "C":
+				display.innerText = "";
+				break;
+			case "=":
+				try {
+					display.innerText = eval(display.innerText);
+				} catch {
+					display.innerText = "Error";
+				}
+				break;
+			case "-":
+				if (display.innerText) {
+					display.innerText = display.innerText(0, -1);
+				}
+				break;
+			default:
+				display.innerText += e.target.innerText;
+		}
+	});
+});
